cbebc39577bdffbd048c4b9c27c097cc2d844203
Lusa <jasminely428@gmail.com>
2024-11-27 12:40:10 -0500

    Started music shuffler

--- Assets/Scripts/PersistentGame/AudioManager.cs
+++ Assets/Scripts/PersistentGame/AudioManager.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioSource audioSource;
+
+    public List<AudioClip> clipList;
+
+    int currSong;
+    float currLength;
+    float downtimeSilence;
+    float currDownTime;
+    private void Start()
     {
-        
+        downtimeSilence = 10f;
+        currDownTime = downtimeSilence;
+        ShufflePlaylist();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (currLength > 0)
+        {
+            currLength -= Time.deltaTime;
+        } else
+        {
+            ShufflePlaylist();
+        }
+    }
+
+    private void ShufflePlaylist()
+    {
+        int song = 0;
+        //if song and currSong are the same, reshuffle the value until we get something different
+        while(song == currSong)
+        {
+            song = Random.Range(0, clipList.Count);
+
+        }
+
+        audioSource.clip = clipList[song];  
+        currLength = clipList[song].length; 
+
     }
 }
