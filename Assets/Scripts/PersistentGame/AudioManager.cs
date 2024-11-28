13fbb4f487458a40be8c6132a7f716fc94db4f36
Lusa <jasminely428@gmail.com>
2024-11-28 00:26:16 -0500

    Update AudioManager.cs

--- Assets/Scripts/PersistentGame/AudioManager.cs
+++ Assets/Scripts/PersistentGame/AudioManager.cs
@@ -11,12 +11,14 @@ public class AudioManager : MonoBehaviour
     static int currSong;
     static float currLength;
     static float stopPoint;
+  
     float downtimeSilence;
     float currDownTime;
     private void Start()
     {
         downtimeSilence = 5f;
         currDownTime = downtimeSilence;
+    
         ShufflePlaylist();
     }
 
@@ -24,12 +26,9 @@ public class AudioManager : MonoBehaviour
     {
         if (currLength > 0)
         {
+            
             currLength -= Time.deltaTime;
             stopPoint += Time.deltaTime;
-        } else if (currDownTime > 0)
-        {
-            stopPoint = 0;
-            currDownTime -= Time.deltaTime;
         } else
         {
             ShufflePlaylist();
@@ -47,25 +46,29 @@ public class AudioManager : MonoBehaviour
         {
             audioSource.clip = clipList[currSong];
             audioSource.time = stopPoint;
-        }
-
-        int song = Random.Range(0, clipList.Count);
-        //if song and currSong are the same, reshuffle the value until we get something different
-        while (song == currSong)
+        } else
         {
-            song = Random.Range(0, clipList.Count);
+            int song = Random.Range(0, clipList.Count);
+            //if song and currSong are the same, reshuffle the value until we get something different
+            while (song == currSong)
+            {
+                song = Random.Range(0, clipList.Count);
 
+            }
+
+            stopPoint = 0;
+            currSong = song;
+
+            audioSource.clip = clipList[currSong];
+            audioSource.Play();
+            audioSource.volume = 0;
+
+            StartCoroutine(AudioFadeout.StartFade(audioSource, 3f, 1));
+
+            currLength = clipList[song].length;
+            currDownTime = downtimeSilence;
         }
 
-        currSong = song;
-
-        audioSource.clip = clipList[currSong];
-        audioSource.Play();
-        audioSource.volume = 0;
-
-        StartCoroutine(AudioFadeout.StartFade(audioSource, 3f, 1));
-
-        currLength = clipList[song].length;
-        currDownTime = downtimeSilence;
+        
     }
 }
